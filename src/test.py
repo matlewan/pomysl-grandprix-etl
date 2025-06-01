456f9549e030f97cb76538be5a757fceca3a086f
matlewan <matlewan7@gmail.com>
2025-06-01 20:50:13 +0200

    fix: replace unicode escapes with utf-8 polish characters

--- src/test.py
+++ src/test.py
@@ -1,11 +1,16 @@
 import os
 
-files = os.listdir('data/pomysl-grand-prix/tournaments')
+os.chdir('data/pomysl-grand-prix/tournaments')
+files = os.listdir()
 
 # Otwórz plik i wczytaj dane jako string z sekwencjami \uXXXX
 for filename in files:
+    print(filename)
     with open(filename, "r", encoding="utf-8") as f:
         data = f.read()
+
+    if 'ł' in data:
+        continue
 
     # Zdekoduj unicode escape
     data = data.replace('\\"', '__QUOTE__')
